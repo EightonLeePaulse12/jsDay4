acc1465c3286b623f08989edf95b1d5b2f1ad969
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-05-17 09:34:42 +0200

    Day 4

---
+++ script/code.js
@@ -0,0 +1,19 @@
+let data = {
+    name: "Jack",
+    surname: "Frost",
+    age: 16,
+    address: "10 Daffodail MP"
+}
+
+let data2 = {
+    name:"Santa",
+    surname:"Clause",
+    age:45,
+    address: "North Pole"
+}
+
+console.log(data)
+console.log(data2)
+
+let theData = [data,data2]
+console.log(theData)
